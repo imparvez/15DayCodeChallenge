86fc6c7ce3e1da4bd1df3cddf74a2a3bbbfef234
Parvez Shaikh <parvezshaikhj@gmail.com>
2017-12-04 00:38:49 +0530

    Filter App in JavaSCript and Jquery completed

--- FilterApp/jquery-filter-app/script.js
+++ FilterApp/jquery-filter-app/script.js
@@ -1,6 +1,6 @@
 var fruitArray = ['apple', 'grapes', 'banana', 'watermelon', 'manoges']; // Define the array
-var searchInput = document.getElementById('searchInput'); // target the input box
-var resultData = document.getElementById('resultData'); // Get the container in which the array's list will be inserted
+var searchInput = $('#searchInput'); // target the input box
+var resultData = $('#resultData'); // Get the container in which the array's list will be inserted
 
 /************Render part is done***************/
 var renderList = function(fruitArray){
@@ -8,21 +8,20 @@ var renderList = function(fruitArray){
     for(index in fruitArray){
         li += "<li>"+fruitArray[index]+"</li>";
     }
-    resultData.innerHTML = li;
+    resultData.html(li);
 }
 
 renderList(fruitArray);
 
 /************Filter***************/
-var filterUsers = function(event){
-    var value = searchInput.value;
-    
+
+searchInput.keyup(function(event){
+    var value = searchInput.val();
+
     filterUsers = fruitArray.filter(function(fruits){
         var fruits = fruits.toLowerCase();
         return fruits.indexOf(value) > -1;
     });
 
     renderList(filterUsers);
-}
-
-searchInput.addEventListener('keyup', filterUsers);
+});
